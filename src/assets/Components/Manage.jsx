703ea63619fb7e84c181db8edd454fda69677578
Milena Oswald <milena.oswald@icloud.com>
2024-03-25 15:30:47 +0100

    css cleanup

--- src/assets/Components/Manage.jsx
+++ src/assets/Components/Manage.jsx
@@ -5,47 +5,57 @@ export default function Manage() {
   return (
   <>
       <section className="manage-budgeting">
-        <div className="manage-budgeting-textbox">
-          <h2>Budgeting with N26—helping you manage and save your money</h2>
-          <button rel="stylesheet" href="">Open Bank Account</button>
+        <div className="manage-textbox">
+        <div className="manage-text-h2">
+          <h2 id="manage-first-h2">Budgeting with N26—helping you manage and save your money</h2>
+          <button id="manage-budgeting-button" rel="stylesheet" href="">Open Bank Account</button>
           </div>
-        <img src="https://images.ctfassets.net/q33z48p65a6w/13ouk11cdAFClaxxFd4I02/3b3f3ede2bfa3411ea4f4879b08837d0/CRST-1667_TIHIB_Web_Insights_textmedia_intro-subject_to_crop_1400x1400_EN.png?bg=rgb%3A48ac98&fit=pad&fm=webp&h=1300&q=70&w=1300" alt="man sitting" />
+          </div>
+        <img className="manage-img" src="https://images.ctfassets.net/q33z48p65a6w/13ouk11cdAFClaxxFd4I02/3b3f3ede2bfa3411ea4f4879b08837d0/CRST-1667_TIHIB_Web_Insights_textmedia_intro-subject_to_crop_1400x1400_EN.png?bg=rgb%3A48ac98&fit=pad&fm=webp&h=1300&q=70&w=1300" alt="man sitting" />
       </section>
       <section className="manage-push-notification">
-        <div className="manage-push-notification-wrapper">
-          <div className="manage-push-notification-textbox">
-            <h2>Instant push-notification–you don’t wanna miss a thing</h2>
-            <p>Thanks to the N26 app’s push-notifications, you’ll always be in the know of where your money is going. Receive push-notifications immediately after all transactions, including card and mobile payments, ATM withdrawals, direct debits and transfers.</p>
-            <button rel="stylesheet" href="">Open Online Bank Account   ›</button>
+        <div className="manage-wrapper">
+          <div className="manage-textbox">
+            <div className="manage-text">
+            <h2 className="manage-h2">Instant push-notification–you don’t wanna miss a thing</h2>
+            <p className="manage-p">Thanks to the N26 app’s push-notifications, you’ll always be in the know of where your money is going. Receive push-notifications immediately after all transactions, including card and mobile payments, ATM withdrawals, direct debits and transfers.</p>
+            <button id="push-notification-button" rel="stylesheet" href="">Open Online Bank Account   ›</button>
+            </div>
           </div>
-          <img className="manage-push-notification" src="https://images.ctfassets.net/q33z48p65a6w/ZIN4jyXPfbkFyiESkHARd/230e31113f0d75fffc1cc0dd385de6d0/CRST-1667_TIHIB_Web_Insights_textmedia_02-background_1400x1400_EN.png?bg=rgb%3Af2f2f2&fit=pad&fm=webp&h=1000&q=70&w=1000" alt="nail polish" />
+          <img className="manage-img" src="https://images.ctfassets.net/q33z48p65a6w/ZIN4jyXPfbkFyiESkHARd/230e31113f0d75fffc1cc0dd385de6d0/CRST-1667_TIHIB_Web_Insights_textmedia_02-background_1400x1400_EN.png?bg=rgb%3Af2f2f2&fit=pad&fm=webp&h=1000&q=70&w=1000" alt="nail polish" />
           </div>
       </section>
       <section className="manage-limits">
-        <div className="manage-limits-wrapper">
-          <video autoplay loop>
+        <div className="manage-wrapper">
+          <video className="manage-img" autoplay loop>
             <source type="video/mp4" src="https://videos.ctfassets.net/q33z48p65a6w/3vnAYbWikYh9T9kL6iwG2G/eb2a69c863b0242f63a07d1b8d50dbbc/CRST-1667_TIHIB_Web_Insights_textmedia_01-centered_rounded_borders_700x700_EN.mp4"></source>
           </video>
-          <div className="manage-limits-textbox">
-            <h2 className="manage-component">Spending limits—you’ve got the power</h2>
-            <p>Set daily spending limits, lock or unlock your card and reset your PIN anywhere in the world, 24/7 and directly from the N26s app. After all, you’re in control and it’s you that decides how much you spend, and what you spend your money on.</p>
+          <div className="manage-textbox">
+          <div className="manage-text">
+            <h2 className="manage-h2">Spending limits—you’ve got the power</h2>
+            <p className="manage-p">Set daily spending limits, lock or unlock your card and reset your PIN anywhere in the world, 24/7 and directly from the N26s app. After all, you’re in control and it’s you that decides how much you spend, and what you spend your money on.</p>
+          </div>
           </div>
         </div>
       </section>
       <section className="manage-spending">
-        <div className="manage-spending-wrapper">
-          <div className="manage-spending-textbox">
-            <h2>Master your spending with Insights</h2>
-            <p>Ready for a simple and quick view of your finances? Meet N26 Insights, the intelligent feature in your N26 app that automatically categorizes your payments in real time. Instantly see if you’re spending more than usual on ‘Shopping,’ ‘Bars & Restaurants,’ ‘Groceries,’ and more—all it takes is a quick glance to stay on track with your monthly budget. Plus, with a clear overview of your upcoming regular payments, it’s never been easier to plan ahead and stay on top of your finances.</p>
+        <div className="manage-wrapper">
+          <div className="manage-textbox">
+          <div className="manage-text">
+            <h2 className="manage-h2">Master your spending with Insights</h2>
+            <p className="manage-p">Ready for a simple and quick view of your finances? Meet N26 Insights, the intelligent feature in your N26 app that automatically categorizes your payments in real time. Instantly see if you’re spending more than usual on ‘Shopping,’ ‘Bars & Restaurants,’ ‘Groceries,’ and more—all it takes is a quick glance to stay on track with your monthly budget. Plus, with a clear overview of your upcoming regular payments, it’s never been easier to plan ahead and stay on top of your finances.</p>
           </div>
-          <video src="https://videos.ctfassets.net/q33z48p65a6w/mJWCbY1Lcuz3JXSeUSlH9/8092a0018a5a98141d5dcdbe7a2a071a/2105_Statisitc-Web-Module_WheatBG_1400x1400_EN_V01.mp4"></video>
+          </div>
+          <video className="manage-img" src="https://videos.ctfassets.net/q33z48p65a6w/mJWCbY1Lcuz3JXSeUSlH9/8092a0018a5a98141d5dcdbe7a2a071a/2105_Statisitc-Web-Module_WheatBG_1400x1400_EN_V01.mp4"></video>
           </div>
       </section>
       <section className="manage-spaces">
-        <img src="https://images.ctfassets.net/q33z48p65a6w/39ALVVaiX169HN5Kf69KD6/5547668e20388cd64af75fb3e3a6c446/2204_Spaces_Rebrand_Budgeting_LP_Module4_700x700_EN.png?bg=rgb%3Affffff&fit=pad&fm=webp&h=1002&q=70&w=1000" alt="cards" />
-        <div className="manage-spaces-textbox">
-          <h2 className="manage-component">Spaces—the starting point to budget all your plans</h2>
-          <p>N26 Spaces lets you create sub-accounts that allow you to easily budget and save money. These sit alongside your main N26 account, letting you transfer money between them instantly, budget wisely and put funds aside by separating your savings from expenses.</p>
+        <img className="manage-img" src="https://images.ctfassets.net/q33z48p65a6w/39ALVVaiX169HN5Kf69KD6/5547668e20388cd64af75fb3e3a6c446/2204_Spaces_Rebrand_Budgeting_LP_Module4_700x700_EN.png?bg=rgb%3Affffff&fit=pad&fm=webp&h=1002&q=70&w=1000" alt="cards" />
+        <div className="manage-textbox">
+        <div className="manage-text">
+          <h2 className="manage-h2">Spaces—the starting point to budget all your plans</h2>
+          <p className="manage-p">N26 Spaces lets you create sub-accounts that allow you to easily budget and save money. These sit alongside your main N26 account, letting you transfer money between them instantly, budget wisely and put funds aside by separating your savings from expenses.</p>
+        </div>
         </div>
       </section>
   </>
